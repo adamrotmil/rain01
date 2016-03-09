d2d19e8163e29e8c0b5181af173c43d5d60dcd75
Adam Rotmil <adamrotmil@Adams-iMac.local>
2016-03-09 18:22:48 -0500

    Google geolocation

--- js/app.js
+++ js/app.js
@@ -1,39 +1,47 @@
 $(document).ready(function() {
-	function getImage() {
-		$.get("http://api.giphy.com/v1/gifs/search?q=rain&api_key=dc6zaTOxFJmzC&limit=500")
-		.done(function(data) {
-			console.log("success got data", data.data[0].images.downsized_medium.url);
-			console.log(data);
-
-			var num = Math.floor((Math.random() * 490));
-			console.log("Num is " + num)
-
-			var imageData = data.data[num].images.downsized_medium.url;
-			console.log("Assigned imageData as" + imageData);
-
-			// Append the GIF to the view
-			$('.render-weather').css("background-image", "url(" + imageData + ")");
+	function getWeather() {
+		// convert city name to coordinates
+		$.get("http://maps.googleapis.com/maps/api/geocode/json?address=Paris&sensor=false")
+		.done(function(cityname) {
+			console.log(cityname.results[0].geometry.location);
+			console.log(cityname.results[0].formatted_address);	
+		});
+		// get the weather keyword
+		var apiKey = 'c614101fd6eadbf4e42194a547c4f07b';
+		var url = 'https://api.forecast.io/forecast/';
+		var lati = 25.234479;
+		var longi = 110.179954;
+		var data;
+		$.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
+			if (data.currently.icon == "clear-night") {
+				keyword = "night"
+			} else if (data.currently.icon == "rain") {
+				keyword = "rain"
+			};
+			// get the GIF
+			$.get("http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=500")
+			.done(function(data) {
+				var num = Math.floor((Math.random() * 490));
+				var imageData = data.data[num].images.downsized_medium.url;
+				// Append the GIF to the view
+				$('.render-weather').fadeTo(2000, 0.1, function()
+				{
+					$(this).css("background-image", "url(" + imageData + ")");
+				}).fadeTo('slow', 1);
+			});
 		});
 	};
-	getImage();
-
-	$(document).on('mousedown', '#happy', function() {
-		console.log("clicked happy days");
-		var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=funny&api_key=dc6zaTOxFJmzC&limit=200");
-		xhr.done(function(data) {
-			console.log("success got data", data.data[0].images.downsized_medium.url);
-
-			var num = Math.floor((Math.random() * 100));
-			console.log("Num is " + num)
-
-			var imageData = data.data[num].images.downsized_medium.url;
-			console.log("Assigned imageData as" + imageData);
-
-			// Append the GIF to the view
-
-			$('.render-weather').empty().append(
-				("<img style=\"width: inherit; \" src = " + imageData + " / > ")
-				);
-		});
-	});
+	getWeather();
 });
+
+//////////////////////////////////////////////////////////////// To Do List /////////////////////////////////////////////////////////////////////
+// 
+// - Social Buttons
+// - Set up views where you can store a list of "favorite" cities that you want to know the weather about.
+// - Set up the weather API to detect rain in a given location and have it trigger the GIF.
+// - Convert a city name string into LAT and LONG for getWeather()
+// --- DONE --- Detect if the GIF fills less than 100% of the vertical image area on screen, if so then expand image proportionally and crop to center.
+//
+//
+//
+//
